3afb7089a54d688f7190f34e17bed26a52eb41a1
Kevin Rejko <kevin@nkp.io>
2017-09-29 23:42:00 -0400

    created remaining automated tests for POST /user.

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -5,6 +5,7 @@ from tests import BaseTestCase, DATA_KEY, ERRORS_KEY
 
 
 class FunctionalTestCase (BaseTestCase):
+    """Test case base class that contains db setup and request checking for functional test cases"""
 
     def setUp(self):
         db.create_all()
@@ -19,15 +20,15 @@ class FunctionalTestCase (BaseTestCase):
         db.session.commit()
 
     # All global response
-    def check_response_success_and_headers(self, response):
-        self.check_response_code(response, 200)
+    def check_response_code_and_headers(self, response, expected_code):
+        self.check_response_code(response, expected_code)
         self.check_response_headers_content_type(response)
 
     def check_response_headers_content_type(self, response):
         self.assertEquals(response.headers['Content-Type'], 'application/json')
 
-    def check_response_code(self, response, code):
-        self.assertEquals(response.status_code, code)
+    def check_response_code(self, response, expected_code):
+        self.assertEquals(response.status_code, expected_code, "response code not " + str(expected_code))
 
     # Unpack extant response values
     def unpack_extant_response_data(self, response):
@@ -51,20 +52,21 @@ class FunctionalTestCase (BaseTestCase):
         self.assertNotIn(key, response.data)
 
     # Convenience checks
-    def check_request_contains_only_auth_error(self, response):
+    def check_response_contains_only_auth_error(self, response):
         # TODO Define error codes
-        self.check_request_contains_only_error(101, response)
+        self.check_response_contains_only_error(101, response)
 
-    def check_request_contains_only_missing_parameter_error(self, response):
+    def check_response_contains_only_missing_parameter_error(self, response):
         # TODO Define error codes
-        self.check_request_contains_only_error(100, response)
+        self.check_response_contains_only_error(100, response)
 
-    def check_request_contains_only_error(self, code, response):
+    def check_response_contains_only_error(self, expected_code, response):
+        """Validates no data exists, validates errors array does exist and checks for expected error code."""
         self.check_data_not_in_response(response)
         error_arr = self.unpack_extant_response_errors(response)
-        self.check_for_error(code, error_arr)
+        self.__check_for_error(expected_code, error_arr)
 
-    def check_for_error(self, error_code, error_arr):
+    def __check_for_error(self, error_code, error_arr):
         self.assertGreater(len(error_arr), 0)
         for error in error_arr:
             code = error['code']
---
+++ tests/functional/user_create_test.py
@@ -0,0 +1,68 @@
+import json
+from tests.functional import FunctionalTestCase
+from tests.mocks.user_mock import TEST_PASSWORD, TEST_USERNAME, mock_user
+from tests.functional.constant_keys import USERNAME_KEY, PASSWORD_KEY
+from app.dao.user_dao import get_user_from_username
+from app.models.user import User
+from tests.functional.constants import *
+
+
+
+MAX_PASSWORD_LENGTH = 256
+MIN_PASSWORD_LENGTH = 6
+MAX_USERNAME_LEGNTH = 32
+MIN_USERNAME_LENGTH = 3
+LONG_PASSWORD = "LoremipsumdolorsitametconsecteturadipiscingelitPhasellusfeugiataliqueturnaeuconvallisNuncutpurussollicitudinmollisnibhnoninterdummagnaDonecvestibulumrhoncusnullasedlaoreetmassafeugiatvelNullamloremvelitdignissimsedsemneclobortisauctornisiNullahendreritsemno"
+
+
+class UserCreateTestCase(FunctionalTestCase):
+    """This class is used to test all inputs and outputs of the POST /user endpoint"""
+
+    # Successful Check
+    def test_user_create_success(self):
+
+        request_params = {USERNAME_KEY: TEST_USERNAME, PASSWORD_KEY: TEST_PASSWORD}
+        response = self.client.post('/user', data=json.dumps(request_params))
+
+        user = get_user_from_username(TEST_USERNAME)
+        self.assertIsNotNone(user, "created user not found")
+        self.assertTrue(user.check_hashed_password(TEST_PASSWORD))
+        self.check_response_code_and_headers(response, HTTP_CODE_201_CREATED)
+
+    # Input Validation
+    def test_user_create_username_too_short_failure(self):
+        request_params = {USERNAME_KEY: "no", PASSWORD_KEY: TEST_PASSWORD}
+        self.make_and_check_user_create_attempt_with_error(request_params, 901)
+
+    def test_user_create_password_too_short_failure(self):
+        request_params = {USERNAME_KEY: TEST_USERNAME, PASSWORD_KEY: "short"}
+        self.make_and_check_user_create_attempt_with_error(request_params, 902)
+
+    def test_user_create_password_too_long_failure(self):
+        request_params = {USERNAME_KEY: TEST_USERNAME, PASSWORD_KEY: LONG_PASSWORD}
+        self.make_and_check_user_create_attempt_with_error(request_params, 903)
+
+    def test_user_create_username_contains_spaces_failures(self):
+        request_params = {USERNAME_KEY: "contains spaces", PASSWORD_KEY: TEST_PASSWORD}
+        self.make_and_check_user_create_attempt_with_error(request_params, 904)
+
+    def make_and_check_user_create_attempt_with_error(self, request_params, app_error_code):
+        response = self.client.post('/user', data=json.dumps(request_params))
+        user = get_user_from_username(request_params[USERNAME_KEY])
+        self.assertIsNone(user)
+        self.check_response_code_and_headers(response, HTTP_CODE_422_UNPROCESSABLE_ENTITY)
+        self.check_response_contains_only_error(app_error_code, response)
+
+    # Conflict Check
+    def test_user_create_username_taken_failure(self):
+        user = mock_user()
+        self.save_objects(user)
+        request_params = {USERNAME_KEY: user.username, PASSWORD_KEY: TEST_PASSWORD}
+        response = self.client.post('/user', data=json.dumps(request_params))
+        self.check_response_code_and_headers(response, HTTP_CODE_409_CONFLICT)
+        self.check_response_contains_only_error(900, response)
+
+
+
+
+
