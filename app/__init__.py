29a129770068f212a2f9c23f7e7be03e459c71b3
Kevin Rejko <kevin@nkp.io>
2017-10-08 16:46:23 -0400

    Added app factory which assigns app to db and creats db tables

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,34 @@
 
 from flask import Flask, g, request
+
+# DB Models
 from app.models import db
+from app.models.user import User
+from app.models.authorization import Authorization
+
+# Blueprints
 from app.controllers.auth.login import auth_blueprint
 from app.controllers.user.create_user import user_blueprint
 
 
-# Define the WSGI application object and configurations
-app = Flask(__name__)
-app.config.from_object('config.config')
+def create_app():
+    # Define the WSGI application object and configurations
+    app = Flask(__name__)
+    app.config.from_object('config.config')
 
-# Define the database object which is imported by modules and controllers
-# (See http://stackoverflow.com/a/9695045/604003 for explanation)
-db.init_app(app)
+    # Define the database object which is imported by modules and controllers
+    # (See http://stackoverflow.com/a/9695045/604003 for explanation)
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+
+    return app
+
+# Create an app and its endpoints
+app = create_app()
+app.register_blueprint(auth_blueprint)
+app.register_blueprint(user_blueprint)
+
 
 # Initialize response objects for controller requests
 @app.before_request
@@ -19,8 +36,3 @@ def before_request():
     # Initialize a response
     g.response = {}
     g.request_params = request.get_json(force=True)
-
-
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(user_blueprint)
-
